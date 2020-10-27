a595244944ca50b9beddb9e001851028a5154aa8
jatinjiogarg <jatinjiogarg@gmail.com>
2020-10-27 15:31:49 +0530

    Home page improved

--- Covid19/dashboard/views.py
+++ Covid19/dashboard/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
-
+from django import forms
+from django.http import HttpResponse
 # Create your views here.
 
+      
+
 def dashboard(request):
+    if(request.method=='POST'):
+           return HttpResponse(request.post)
+            
     return render(request,"dashboard/base.html")
--- Covid19/home/urls.py
+++ Covid19/home/urls.py
@@ -3,5 +3,4 @@ from . import views
 
 urlpatterns = [
     path("", views.home,name="hello"),
-    path("data",views.get_data,name="get_data")
 ]
